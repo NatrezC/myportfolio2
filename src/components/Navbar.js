bce0c9967ffc5fb82324b88be14b51370309d40c
NatrezC <cnatrez@gmail.com>
2020-12-23 21:45:48 -0600

    created to where if the screen is smaller it will resize

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,21 @@ import { Link } from 'react-router-dom'
 
 function Navbar() {
     const [click, setClick] = useState(false)
+    const [button, setButton] = useState(true)
     
     const handleClick = () => setClick(!click)
     const closeMobileMenu = () => setClick(false)
+
+    const showButton = () => {
+        if (window.innderWidth <= 960) {
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    };
+
+    window.addEventListener('resize', showButton)
+    
     return (
         <>
             <nav className="navbar">
@@ -44,6 +56,7 @@ function Navbar() {
                             </Link>
                         </li>
                     </ul>
+                    {button && <Button buttonStyle="btn--outline">Sign up</Button>}
                 </div>
             </nav>
         </>
