f0c62347a36bc84931636afd45e5a1277e3636f2
NatrezC <cnatrez@gmail.com>
2020-12-24 19:32:18 -0600

    created Hero component

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import {Button} from './Button'
+import '../App.css'
+import { Button } from './Button'
+import './HeroSection.css'
 
 function HeroSection() {
     return (
@@ -10,6 +12,9 @@ function HeroSection() {
             <div className="hero-btns">
                 <Button className='btns' buttonStyle="btn--outline" buttonSize="btn--large">
                     GET STARTED
+                </Button>
+                <Button className='btns' buttonStyle="btn--primary" buttonSize="btn--large">
+                    Prolly DONT NEED <i className = "far fa-play-circle" />
                 </Button>
             </div>
         </div>
