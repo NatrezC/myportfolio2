5c8bf16bea16b9cb8c515ce6b0219ca1f5cf8e99
NatrezC <cnatrez@gmail.com>
2020-12-24 19:42:58 -0600

    video plays on main page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import './App.css'
 import {BrowserRouter, Switch, Route} from 'react-router-dom'
+import Home from './components/pages/Home'
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <BrowserRouter>
         <Navbar/>
         <Switch>
-          <Route exact path="/"></Route>
+          <Route exact path="/" component={Home}></Route>
         </Switch>
       </BrowserRouter>
     </>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,1 +1,13 @@
-impor
+import React from 'react'
+import '../../App.css'
+import HeroSection from '../HeroSection'
+
+function Home() {
+    return (
+        <>
+            <HeroSection />
+        </>
+    )
+}
+
+export default Home;
